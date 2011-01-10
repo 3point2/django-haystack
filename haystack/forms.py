655424c9b1d33b8d785203451cde1fc91fe8d809
Vasili <vasili@techcollective.com>
2011-01-09 17:48:39 -0800

    returning empty SQS if form is invalid

--- haystack/forms.py
+++ haystack/forms.py
@@ -9,52 +9,51 @@ from haystack.query import SearchQuerySet, EmptySearchQuerySet
 def model_choices(site=None):
     if site is None:
         site = haystack.site
-    
+
     choices = [("%s.%s" % (m._meta.app_label, m._meta.module_name), capfirst(unicode(m._meta.verbose_name_plural))) for m in site.get_indexed_models()]
     return sorted(choices, key=lambda x: x[1])
 
 
 class SearchForm(forms.Form):
     q = forms.CharField(required=False, label=_('Search'))
-    
+
     def __init__(self, *args, **kwargs):
         self.searchqueryset = kwargs.pop('searchqueryset', None)
         self.load_all = kwargs.pop('load_all', False)
-        
+
         if self.searchqueryset is None:
             self.searchqueryset = SearchQuerySet()
-        
+
         super(SearchForm, self).__init__(*args, **kwargs)
-    
+
     def no_query_found(self):
         """
         Determines the behavior when no query was found.
-        
+
         By default, no results are returned (``EmptySearchQuerySet``).
-        
+
         Should you want to show all results, override this method in your
         own ``SearchForm`` subclass and do ``return self.searchqueryset.all()``.
         """
         return EmptySearchQuerySet()
-    
+
     def search(self):
         if not self.is_valid():
-            return self.no_query_found()
-        
+            return EmptySearchQuerySet()
+
         if not self.cleaned_data['q']:
             return self.no_query_found()
-        
         sqs = self.searchqueryset.auto_query(self.cleaned_data['q'])
-        
+
         if self.load_all:
             sqs = sqs.load_all()
-        
+
         return sqs
-    
+
     def get_suggestion(self):
         if not self.is_valid():
             return None
-        
+
         return self.searchqueryset.spelling_suggestion(self.cleaned_data['q'])
 
 
@@ -65,13 +64,13 @@ class HighlightedSearchForm(SearchForm):
 
 class FacetedSearchForm(SearchForm):
     selected_facets = forms.CharField(required=False, widget=forms.HiddenInput)
-    
+
     def search(self):
         sqs = super(FacetedSearchForm, self).search()
-        
+
         if hasattr(self, 'cleaned_data') and self.cleaned_data['selected_facets']:
             sqs = sqs.narrow(self.cleaned_data['selected_facets'])
-        
+
         return sqs
 
 
@@ -83,13 +82,13 @@ class ModelSearchForm(SearchForm):
     def get_models(self):
         """Return an alphabetical list of model classes in the index."""
         search_models = []
-        
+
         if self.is_valid():
             for model in self.cleaned_data['models']:
                 search_models.append(models.get_model(*model.split('.')))
-        
+
         return search_models
-    
+
     def search(self):
         sqs = super(ModelSearchForm, self).search()
         return sqs.models(*self.get_models())
@@ -102,11 +101,11 @@ class HighlightedModelSearchForm(ModelSearchForm):
 
 class FacetedModelSearchForm(ModelSearchForm):
     selected_facets = forms.CharField(required=False, widget=forms.HiddenInput)
-    
+
     def search(self):
         sqs = super(FacetedModelSearchForm, self).search()
-        
+
         if hasattr(self, 'cleaned_data') and self.cleaned_data['selected_facets']:
             sqs = sqs.narrow(self.cleaned_data['selected_facets'])
-        
+
         return sqs.models(*self.get_models())
